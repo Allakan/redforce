0531797202282b61d06877b75f78731ac9482f65
Allakan <burukreekan@mail.ru>
2023-05-23 15:30:59 +0300

    Add modified projectss

--- myproject/urls.py
+++ myproject/urls.py
@@ -14,16 +14,28 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import include, path
-from rest_framework import routers
-from myapp import views
-
-router = routers.DefaultRouter()
+from django.urls import path
+from myapp.views import AdvantagesList, AdvantagesDetail
+from myapp.views import AdvantagesImageList, AdvantagesImageDetail
+from myapp.views import FeedbackList, FeedbackDetail
+from myapp.views import FaqList, FaqDetail
+from myapp.views import ContactsList, ContactsDetail
+from myapp.views import RatingList, RatingDetail
+from myapp.views import SubscribptionList, SubscribptionDetail
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include(router.urls)),
-    path('', include('myapp.urls')),
+    path('advantages/', AdvantagesList.as_view(), name='advantages_list'),
+    path('advantages/<int:pk>/', AdvantagesDetail.as_view(), name='advantages_detail'),
+    path('advantagesimage/', AdvantagesImageList.as_view(), name='advantagesimage_list'),
+    path('advantagesimage/<int:pk>/', AdvantagesImageDetail.as_view(), name='advantagesimage_detail'),
+    path('feedback/', FeedbackList.as_view(), name='feedback_list'),
+    path('feedback/<int:pk>/', FeedbackDetail.as_view(), name='feedback_detail'),
+    path('faq/', FaqList.as_view(), name='faq_list'),
+    path('faq/<int:pk>/', FaqDetail.as_view(), name='faq_detail'),
+    path('contacts/', ContactsList.as_view(), name='contacts_list'),
+    path('contacts/<int:pk>/', ContactsDetail.as_view(), name='contacts_detail'),
+    path('rating/', RatingList.as_view(), name='rating_list'),
+    path('rating/<int:pk>/', RatingDetail.as_view(), name='rating_detail'),
+    path('subscribption/', SubscribptionList.as_view(), name='subscribption_list'),
+    path('subscribption/<int:pk>/', SubscribptionDetail.as_view(), name='subscribption_detail'),
 ]
-
